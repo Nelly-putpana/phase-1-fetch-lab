b071f397b8f012bbe1209bad1b962e90d9d2a3e2
Nelly Putpana <nelly.putpana@gmail.com>
2023-06-11 10:31:25 -0600

    fetch lab final

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-async function asyncfetchBooks() {
+function fetchBooks() {
   const url = "https://anapioficeandfire.com/api/books"
-  const response = await fetch(url).then(r=>r.json())
- 
-  renderBooks (response)
-
+  fetch (url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then (function (books) {
+    renderBooks(books);
+  });
+  return fetch(url);
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  asyncfetchBooks();
-});
+    const main = document.querySelector('main');
+    books.forEach(book => {
+      const h2 = document.createElement('h2');
+      h2.innerHTML = book.name;
+      main.appendChild(h2);
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+      fetchBooks();
+    });
